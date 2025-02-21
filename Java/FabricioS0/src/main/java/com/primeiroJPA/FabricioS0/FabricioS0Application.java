08307c70922f334a405332729b88ccbc35f6a3bf
FabricioS0 <fsoliveira899@gmail.com>
2025-02-21 15:17:38 -0300

    8º Update - JPA

---
+++ Java/FabricioS0/src/main/java/com/primeiroJPA/FabricioS0/FabricioS0Application.java
@@ -0,0 +1,13 @@
+package com.primeiroJPA.FabricioS0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricioS0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricioS0Application.class, args);
+	}
+
+}
